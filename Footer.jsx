64e46a62d7b51e83e51363d8cdc21e7da7763efb
Gene Yee <99918701+KuyaGene23@users.noreply.github.com>
2023-02-22 13:32:34 -0800

    Create Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <React.Fragment>
+      <footer className="container">
+        <p>&copy; Application Project 2022 by Gene H. Yee</p>
+      </footer>
+    </React.Fragment>
+  );
+}
+
+export default Footer;
